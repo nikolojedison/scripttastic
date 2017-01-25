48aa21065ebc22acb5776f7109582a47a6e06cce
Nik <nikolojedison@users.noreply.github.com>
2017-01-25 17:14:48 -0600

    ch-ch-check it

---
+++ back-scratcher.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#a simple backup script for ccdc#
+#add directories as required to DATA with the format /[path]/[to]/[dir]/#
+ 
+DATA="/home /root /etc"
+ 
+#choose where you want to pipe the backup to below#
+tar cfzp "/home/back_scratcher.tgz" $DATA --same-owner
