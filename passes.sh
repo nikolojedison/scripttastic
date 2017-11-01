caf854c6af3d58441c609ee3ce5e099ec8dc9026
Nik <nikolojedison@users.noreply.github.com>
2017-11-01 14:56:52 -0500

    Update passes.sh

--- passes.sh
+++ passes.sh
@@ -3,4 +3,4 @@
 #TODO: make the passwords fancy#
 
 cat /etc/passwd | cut -f 1 -d: > ~/users.txt
-for i in `cat users.txt`;do echo -e "Andshesbuying4stairway2Heaven\nAndshesbuying4stairway2Heaven" | passwd $i; done
+for i in `cat users.txt`;do echo -e "passwd\npasswd" | passwd $i; done
