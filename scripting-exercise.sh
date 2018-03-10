834b4bbd776cf5efb50bff016591f433b754b539
Nik <nikolojedison@users.noreply.github.com>
2018-03-10 14:57:19 -0600

    Update scripting-exercise.sh

--- scripting-exercise.sh
+++ scripting-exercise.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 NAME=`(uname -a)`
-MSG=`(dmesg)`
+MSG=`(dmesg | tail -n 10)`
 ROOT=`(ls -a /)`
 WEB=`(ls -a /var/www/)`
 HOME=`(ls -a /home/)`
@@ -12,8 +12,8 @@ echo "Root files: ${ROOT}"
 echo "Web-facing files: ${WEB}"
 echo "Home files: ${HOME}"
 
-echo "Name: ${NAME}" >> scripting-exercise.txt
-echo "dmesg: ${MSG}" >> scripting-exercise.txt
-echo "Root files: ${ROOT}" >> scripting-exercise.txt
-echo "Web-facing files: ${WEB}" >> scripting-exercise.txt
-echo "Home files: ${HOME}" >> scripting-exercise.txt
+echo "Name: ${NAME}" >> sysinfo.log
+echo "dmesg: ${MSG}" >> sysinfo.log
+echo "Root files: ${ROOT}" >> sysinfo.log
+echo "Web-facing files: ${WEB}" >> sysinfo.log
+echo "Home files: ${HOME}" >> sysinfo.log
